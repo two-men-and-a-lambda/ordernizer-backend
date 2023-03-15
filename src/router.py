46551a4990085f7fecdc1ef4e1b79746cdd0ff37
Ben R Levy <ben.levy@uconn.edu>
2023-03-15 14:32:34 -0700

    testing csv stuff

--- src/router.py
+++ src/router.py
@@ -1,11 +1,26 @@
 import json
 import pandas
+from endpoints import *
 
 def lambda_handler(event, context):
     print(event)
     print(context)
-    return {  #         <---- RETURN THIS RIGHT AWAY 
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! Ben your penis is small')
+    path = event['path'][1:]
+    if path == 'get_totals':
+        result = get_totals()
+    elif path == 'submit_inventory':
+        result = submit_inventory(event['body'])
+    elif path == 'submit_order':
+        result = submit_order(event['body'])
+    elif path == 'submit_sale':
+        result = submit_sale(event['body'])
+    else:
+        return {  #         <---- RETURN THIS RIGHT AWAY 
+        'statusCode': 503,
+        'body': json.dumps(f'{path} is not a valid endpoint')
     }
 
+    return {  #         <---- RETURN THIS RIGHT AWAY 
+        'statusCode': 200,
+        'body': json.dumps(result)
+    }
