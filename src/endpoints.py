6b051bef4dac2c0e5ac707980ae76bcb7269f065
Ben R Levy <ben.levy@uconn.edu>
2023-03-15 22:18:26 -0700

    housekeeping

--- src/endpoints.py
+++ src/endpoints.py
@@ -3,34 +3,19 @@ from io import StringIO
 import boto3
 from main import generate_result
 
-'''
-    - get the totals of each product in stock
-    - uses generate_result() to combine wholesale and retail transactions into one comprehensive dataframe
-    - then finds based on this dataframe what is currently in stock for each product
-'''
 def get_totals(folder='input'):
-    print('before gen result')
     df = generate_result(folder)
-    print('after gen result')
-    sold_out_df = df[df['units_remaining'] == 0]
-    sold_out = list(set(sold_out_df['wholesaleId']))
+    sold_out = list(set(df[df['units_remaining'] == 0]['wholesaleId']))
     in_stock = df[~df['wholesaleId'].isin(sold_out) ]
     totals = in_stock[['wholesaleId', 'product', 'units_remaining']].groupby('wholesaleId').agg({'product': 'max', 'units_remaining': 'min'})
     totals_per_product = totals[['product', 'units_remaining']].groupby('product').sum()
-    totals_dict = totals_per_product.to_dict()['units_remaining']
-    return totals_dict
+    return totals_per_product.to_dict()['units_remaining']
 
-'''
-    - generates a list of lists
-    - this list represents all transactions, which are to then be appended to a dataframe
-'''
-def generate_transactions(order, df, timestamp, totals=None):
+def generate_transactions(order, df, totals):
     transactions = []
+    timestamp = df.pop('timestamp')
     for product, value in order.items():
-        if totals and totals[product] < value['units']:
-            message = f'Error! Not enough {product} in stock'
-            print(message)
-            raise Exception(message)
+        if totals and totals[product] < value['units']: raise Exception(f'Error! Not enough {product} in stock')
         transactions.append([df['id'].max()+1+len(transactions), product, value['price'], value['units'], timestamp])
     return transactions
 
@@ -39,66 +24,27 @@ def append_rows_to_df(transactions, df):
         df.loc[len(df)] = transaction
     return df
 
-'''
-    - takes in a dictionary of inventory per product
-    - based on this inventory, we look at what is currently supposed to be in stock based on the get_totals() function
-    - and then find the discrepancies and log them as transactions with a price = 0
-    ex: 
-        - the get_totals() function returns that we have 8 apples
-        - the store manager takes inventory and finds that they have 3 apples
-        - submit_inventory({'apples': 3}) will record a sale of 5 apples for $0 and mark it as such in retail.csv
-'''
 def get_file(file):
-    s3 = boto3.client('s3')
-    response = s3.get_object(Bucket='ordernizer-database-bucket', Key=file)
-    df = pd.read_csv(response['Body'], sep=',').sort_values(by=['id'])
-    return df
+    response = boto3.client('s3').get_object(Bucket='ordernizer-database-bucket', Key=file)
+    return pd.read_csv(response['Body'], sep=',').sort_values(by=['id'])
 
 def put_file(df, file):
-    bucket = 'ordernizer-database-bucket' # already created on S3
     csv_buffer = StringIO()
     df.to_csv(csv_buffer)
-    s3_resource = boto3.resource('s3')
-    s3_resource.Object(bucket, file).put(Body=csv_buffer.getvalue())
+    boto3.resource('s3').Object('ordernizer-database-bucket', file).put(Body=csv_buffer.getvalue())
+
+def submit_order(sale, file='wholesale', totals=None):
+    input = get_file(f'input/{file}.csv')
+    transactions = generate_transactions(sale, input, totals)
+    output = append_rows_to_df(transactions, input)
+    put_file(output, f'output/{file}.csv')
+    return get_totals('output')
 
 def submit_inventory(new_totals):
-    totals = get_totals('input')
-    timestamp = new_totals.pop('timestamp')
-    diffs = {product: {'price': 0, 'units': totals[product] - units} for product, units in new_totals.items()} 
-    sales = get_file('input/retail.csv')
-    transactions = generate_transactions(diffs, sales, timestamp)
-    sales = append_rows_to_df(transactions, sales)
-    put_file(sales, 'output/retail.csv')
-    result_totals = get_totals('output')
-    return result_totals
-
-
-'''
-    - places an order to the wholesaler
-    - adds the order to wholesale.csv
-'''
-def submit_order(order, wholesale='wholesale.csv'):
-    s3 = boto3.client('s3')
-    response = s3.get_object(Bucket='ordernizer-database-bucket', Key=wholesale)
-    #sales = pd.read_csv(retail).sort_values(by=['id'])
-    batches = pd.read_csv(response['Body'], sep=',').sort_values(by=['id'])
-    timestamp = order.pop('timestamp')
-    transactions = generate_transactions(order, batches, timestamp)
-    batches = append_rows_to_df(transactions, batches)
-    return batches
-
-'''
-    - logs a retail sale
-    - if there is not enough inventory, returns an error and doesn't do anything
-    - else, mark the transaction in retail.csv
-'''
-def submit_sale(sale, retail='retail.csv'):
     totals = get_totals()
-    s3 = boto3.client('s3')
-    response = s3.get_object(Bucket='ordernizer-database-bucket', Key=retail)
-    #sales = pd.read_csv(retail).sort_values(by=['id'])
-    sales = pd.read_csv(response['Body'], sep=',').sort_values(by=['id'])
-    timestamp = sale.pop('timestamp')
-    transactions = generate_transactions(sale, sales, timestamp, totals)
-    sales = append_rows_to_df(transactions, sales)
-    return sales
+    diffs = {product: {'price': 0, 'units': totals[product] - units} for product, units in new_totals.items() if product != 'timestamp'}
+    return submit_order(diffs, 'retail', totals)
+
+def submit_sale(sale):
+    totals = get_totals()
+    return submit_order(sale, 'retail', totals)
--- src/unit_test.py
+++ src/unit_test.py
@@ -1,71 +1,34 @@
 import pandas as pd
 import requests
-import json
 import boto3
 from endpoints import *
 
-def test():
-    r = 'retail_test.csv'
-    w = 'wholesale_test.csv'
-    result = {'generate_result': [generate_result(r, w), pd.read_csv('generate_result.csv')], 'submit_sale': [submit_sale({'bananas': {'price': 10, 'units': 20}, 'apples': {'price': 30, 'units': 23}, 'timestamp': '2023-01-30 04:25:01'}, 'retail_test.csv'), pd.read_csv('submit_sale.csv')], 'submit_order': [submit_order({'bananas': {'price': 10, 'units': 20}, 'apples': {'price': 30, 'units': 100}, 'timestamp': '2023-01-30 04:25:01'}, 'wholesale_test.csv'), pd.read_csv('submit_order.csv')], 'submit_inventory': [submit_inventory({'bananas': 20, 'apples': 23, 'timestamp': '2023-01-30 04:25:01'}, 'retail_test.csv', 'wholesale_test.csv'), pd.read_csv('submit_inventory.csv')]}
-    pass_fail_dict = {}
-    for key, value in result.items():
-        df1 = value[0]
-        df2 = value[1]
-        eq = df1.equals(df2)
-        pass_fail_dict[key] = eq
-        print('*'*50,key,'*'*50)
-        if not eq:
-            print('#'*50, 'FAIL', '#'*50)
-        print('\nWHOLESALE:\n\n',pd.read_csv('wholesale_test.csv'))
-        print('\nRETAIL:\n\n',pd.read_csv('retail_test.csv'))
-        print('\nYOUR ANSWER:\n\n', df1)
-        print('\nCORRECT ANSWER:\n\n', df2)
-    print('\n\n')    
-    for key, value in pass_fail_dict.items():
-        print(key, ' '*(16-len(key)), 'PASS' if value else 'FAIL')
-        
-
-#test()
+def print_stuff(x, file, the_dict):
+    print('*'*25,file,'*'*25)
+    print('PASS' if x else 'FAILURE')
+    for i, df in the_dict.items():
+        print(f'{i.upper()}\n', df)
 
 def test_api():
-    s3 = boto3.resource(
+    bucket = boto3.resource(
         service_name='s3',
         region_name='us-east-1',
         aws_access_key_id='AKIA47ACC4ZGCT6JS5HC',
         aws_secret_access_key='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'
-    )
-    bucket = s3.Bucket('ordernizer-database-bucket')
-    test_dict = {"submit_inventory": {"input": {"bananas": 20, "apples": 23, "timestamp": "2023-01-30 04:25:01"}, "output": {"apples": 23, "bananas": 20}}}
-    #              "submit_order": {"bananas": {"price": 10, "units": 20}, "apples": {"price": 30, "units": 100}, "timestamp": "2023-01-30 04:25:01"},
+    ).Bucket('ordernizer-database-bucket')
+    test_dict = {"submit_inventory": {"input": {"bananas": 20, "apples": 23, "timestamp": "2023-01-30 04:25:01"}, "expected": {"apples": 23, "bananas": 20}},
+                 "submit_order": {"bananas": {"price": 10, "units": 20}, "apples": {"price": 30, "units": 100}, "timestamp": "2023-01-30 04:25:01"}}
     #              "submit_sale": {"bananas": {"price": 10, "units": 20}, "apples": {"price": 30, "units": 23}, "timestamp": "2023-01-30 04:25:01"}
     # }
-    result = {}
-    for endpt, req_body in test_dict.items():
+    for endpt, jsons in test_dict.items():
         print('*'*50,endpt,'*'*50)
-        temp = []
-        raw_resp = requests.post('https://f0nk1usvg2.execute-api.us-east-1.amazonaws.com/'+endpt, json=req_body['input'])
-        resp = raw_resp.json()
-        req_body['expected'] = resp
-        x = int(resp == req_body['output'])
-        temp.append(x)
-        print('*'*25,'TOTALS', '*'*25)
-        print('PASS' if x else 'FAILURE')
-        for i in ['input', 'output', 'expected']:
-            print(f'{i.upper()}\n', req_body[i])
+        jsons['output'] = requests.post('https://f0nk1usvg2.execute-api.us-east-1.amazonaws.com/'+endpt, json=jsons['input']).json()
+        print_stuff(int(jsons['expected'] == jsons['output']), 'JSON OBJECTS', jsons)
         for file in ['wholesale.csv', 'retail.csv']:
             dfs = {}
-            inp_obj = bucket.Object(f'input/{file}').get()
-            dfs['input'] = pd.read_csv(inp_obj['Body'], index_col=0)
-            out_obj = bucket.Object(f'output/{file}').get()
-            dfs['output'] = pd.read_csv(out_obj['Body'], index_col=0)
-            exp_obj = bucket.Object(f'test/{endpt}/{file}').get()
-            dfs['expected'] = pd.read_csv(exp_obj['Body'], index_col=0)
-            x = int(dfs['output'].equals(dfs['expected']))
-
-            temp.append(x)
-            print('*'*25,file,'*'*25)
-            print('PASS' if x else 'FAILURE')
-            for i, df in dfs.items():
-                print(f'{i.upper()}\n', df)
+            for dir in ['input', 'output', f'test/{endpt}']:
+                dfs[dir if dir in ['input', 'output'] else 'expected'] = pd.read_csv(bucket.Object(f'{dir}/{file}').get()['Body'], index_col=0)
+            print_stuff(int(dfs['output'].equals(dfs['expected'])), file, dfs)
+            
 test_api()
+
