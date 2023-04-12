3365aa7e8df13a05e819552d08ba04d9a43ed3c4
Ben R Levy <ben.levy@uconn.edu>
2023-04-11 23:37:47 -0400

    adding get csv endpts

--- src/router.py
+++ src/router.py
@@ -25,10 +25,19 @@ def lambda_handler(event, context):
         result = submit_order(body)
     elif path == 'submit_sale':
         result = submit_sale(body)
+    elif path == 'get_retail_output':
+        result = get_csv('output/retail.csv')
+    elif path == 'get_retail_input':
+        result = get_csv('input/retail.csv')
+    elif path == 'get_wholesale_output':
+        result = get_csv('output/wholesale.csv')
+    elif path == 'get_wholesale_input':
+        result = get_csv('input/wholesale.csv')
     else:
         statusCode = 503
         result = f'{path} is not a valid endpoint'
-
+    print('RESULT:')
+    print(result)
     return { 
         'statusCode': statusCode,
         'body': json.dumps(result)
