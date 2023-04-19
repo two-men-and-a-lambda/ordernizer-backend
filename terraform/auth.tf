6a074533609d9d1dd97d16d8498e47ccff12839e
bigmike0009 <mikea0009@gmail.com>
2023-04-18 22:27:50 -0400

    **NOTE** manual change needed: Cognito module needs to have flag modified for allow non-admin to sign up new user

--- terraform/auth.tf
+++ terraform/auth.tf
@@ -1,9 +1,9 @@
-module "aws_cognito_user_pool_simple_extended_example" {
+module "aws_cognito_user_pool"{
 
   source  = "lgallard/cognito-user-pool/aws"
   version = "0.22.0"
 
-  user_pool_name                                     = "ordernizer_pool"
+  user_pool_name                                     = "ordernizer_auth_pool"
   alias_attributes                                   = ["email", "phone_number"]
   auto_verified_attributes                           = ["email"]
   sms_authentication_message                         = "Your username is {username} and temporary password is {####}."
@@ -13,6 +13,7 @@ module "aws_cognito_user_pool_simple_extended_example" {
   password_policy_minimum_length                     = 6
   user_pool_add_ons_advanced_security_mode           = "OFF"
   verification_message_template_default_email_option = "CONFIRM_WITH_CODE"
+
 
   # schemas
   schemas = [
@@ -35,7 +36,7 @@ module "aws_cognito_user_pool_simple_extended_example" {
 
       string_attribute_constraints = {
         min_length = 7
-        max_length = 15
+        max_length = 150
       }
     },
   ]
@@ -50,11 +51,13 @@ module "aws_cognito_user_pool_simple_extended_example" {
   client_default_redirect_uri                 = "https://ordernizer.org/callback"
   client_read_attributes                      = ["email"]
   client_refresh_token_validity               = 30
+  client_generate_secret                      = false
 
 
   # user_group
-  user_group_name        = "mygroup"
-  user_group_description = "My group"
+  user_group_name        = "ordernizer"
+  user_group_description = "Staying Ordernized"
+  
 
   # ressource server
   resource_server_identifier        = "https://ordernizer.org"
